f687225ff9b06f1767b1b40705f406f930dd164d
Michael Strasser <michael@michaelstrasser.com>
2021-05-25 22:21:45 +1000

    Tidied up serialisation test

--- src/test/kotlin/mjs/kafka/DataTypesTest.kt
+++ src/test/kotlin/mjs/kafka/DataTypesTest.kt
@@ -9,19 +9,17 @@ import kotlinx.serialization.json.Json
 class DataTypesTest : DescribeSpec({
 
     describe("Wrapped customer message") {
+        val message = Wrapper(
+            data = Customer(id = "123456789", customerType = "O"),
+            header = Header(transactionId = "123123123", transactionEventCounter = 1),
+        )
+        val json =
+            """{"data":{"type":"mjs.kafka.Customer","id":"123456789","customerType":"O"},"header":{"transactionId":"123123123","transactionEventCounter":1}}"""
         it("serialises correctly") {
-            val message = Wrapper(
-                data = Customer(id = "123456789", customerType = "O"),
-                header = Header(transactionId = "123123123", transactionEventCounter = 1),
-            )
-            Json.encodeToString(message).shouldBe("""{"data":{"type":"mjs.kafka.Customer","id":"123456789","customerType":"O"},"header":{"transactionId":"123123123","transactionEventCounter":1}}""")
+            Json.encodeToString(message) shouldBe json
         }
         it("deserialises correctly") {
-            val json = """{"data":{"type":"mjs.kafka.Customer","id":"123456789","customerType":"O"},"header":{"transactionId":"123123123","transactionEventCounter":1}}"""
-            Json.decodeFromString<Wrapper>(json).shouldBe(Wrapper(
-                data = Customer(id = "123456789", customerType = "O"),
-                header = Header(transactionId = "123123123", transactionEventCounter = 1),
-            ))
+            Json.decodeFromString<Wrapper>(json) shouldBe message
         }
     }
 
