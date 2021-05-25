8180d93258ecad6e69cd6bee82598cddd9dfce95
Michael Strasser <michael@michaelstrasser.com>
2021-05-25 22:16:58 +1000

    WIP: partial stream aggregation

--- src/main/kotlin/mjs/kafka/Streams.kt
+++ src/main/kotlin/mjs/kafka/Streams.kt
@@ -1,13 +1,16 @@
 package mjs.kafka
 
 import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.kstream.KTable
+import org.apache.kafka.streams.kstream.Materialized
 import org.apache.kafka.streams.kstream.TimeWindows
+import org.apache.kafka.streams.kstream.Windowed
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import java.time.Duration
 import javax.annotation.PostConstruct
 
-@Configuration
+//@Configuration
 //@Enable
 class Streams(
     val builder: StreamsBuilder
@@ -17,16 +20,28 @@ class Streams(
     lateinit var inputStream: String
 
     @PostConstruct
-    fun groupTransactionEvents() =
-        builder.stream<String, Wrapper>(inputStream)
-            .groupByKey()
-            .windowedBy(TimeWindows.of(Duration.ofMinutes(60L)))
-            .aggregate<String, OrgCustomer>(
-                { OrgCustomer() },
-                { key, value, customer ->
-                    when(value.data is )
+    fun groupTransactionEvents(): KTable<Windowed<String>, OrgCustomer> = builder.stream<String, Wrapper>(inputStream)
+        .groupByKey()
+        .windowedBy(TimeWindows.of(Duration.ofMinutes(60L)))
+        .aggregate(
+            { OrgCustomer() },
+            { _, value, customer ->
+                when (value.data) {
+                    is Customer -> {
+                        customer.id = value.data.id
+                    }
+                    is Organisation -> {
+                        customer.id = value.data.customerId
+                        customer.acn = value.data.acn
+                    }
+                    is OrganisationName -> {
+                        customer.id = value.data.customerId
+                        customer.name = value.data.name
+                    }
+                    else -> throw IllegalStateException("Aggregator got OrgCustomer type")
                 }
-            )
-
+                customer
+            },
+        )
 
 }
