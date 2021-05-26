fe7607c579dd94031d38aa273be30f86836d1574
Michael Strasser <mstrass@thoughtworks.com>
2021-05-27 09:44:42 +1000

    KDoc for Serdes code

--- src/main/kotlin/mjs/kafka/Serdes.kt
+++ src/main/kotlin/mjs/kafka/Serdes.kt
@@ -7,12 +7,24 @@ import org.apache.kafka.common.serialization.Deserializer
 import org.apache.kafka.common.serialization.Serde
 import org.apache.kafka.common.serialization.Serializer
 
+/**
+ * Kafka [Serializer] for a Kotlin type that can be serialised using
+ * `kotlinx.serialization`.
+ */
 inline fun <reified T> serializerFor() =
     Serializer<T> { _, data -> Json.encodeToString(data).toByteArray() }
 
+/**
+ * Kafka [Deserializer] for a Kotlin type that can be serialised using
+ * `kotlinx.serialization`.
+ */
 inline fun <reified T> deserializerFor() =
     Deserializer { _, data -> Json.decodeFromString<T>(String(data)) }
 
+/**
+ * Kafka [Serde] for a Kotlin type that can be serialised using
+ * `kotlinx.serialization`.
+ */
 inline fun <reified T> serdeFor() = object : Serde<T> {
     override fun serializer() = serializerFor<T>()
     override fun deserializer() = deserializerFor<T>()
