b8624c91c08685bc674f9911becabeddaf6ed2c2
Michael Strasser <mstrass@thoughtworks.com>
2021-05-26 13:31:10 +1000

    Transaction class is immutable

--- src/main/kotlin/mjs/kafka/Transaction.kt
+++ src/main/kotlin/mjs/kafka/Transaction.kt
@@ -2,26 +2,23 @@ package mjs.kafka
 
 const val NONE = "NONE"
 
-class Transaction {
-    var id: String = NONE
-    val messages = mutableSetOf<Message>()
-    private var partCount: Int = -1
+class Transaction(
+    private val id: String = NONE,
+    val messages: Set<Message> = setOf(),
+    private val partCount: Int = -1,
+) {
     val isComplete: Boolean
         get() = messages.size == partCount
 
     fun addMessage(message: Message): Transaction {
 
-        if (id == NONE)
-            id = message.header.transactionId
-        else if (id != message.header.transactionId)
+        if (id != NONE && id != message.header.transactionId)
             throw IllegalStateException("Attempted to add message with transaction ID ${message.header.transactionId} to transaction $id")
 
-        messages.add(message)
-
-        if (message.header.lastInTransaction) {
-            partCount = message.header.transactionEventCounter
-        }
-
-        return this
+        return Transaction(
+            id = message.header.transactionId,
+            messages = messages + message,
+            partCount = if (message.header.lastInTransaction) message.header.transactionEventCounter else -1
+        )
     }
 }
--- src/test/kotlin/mjs/kafka/TransactionTest.kt
+++ src/test/kotlin/mjs/kafka/TransactionTest.kt
@@ -21,27 +21,23 @@ class TransactionTest : DescribeSpec({
 
     describe("A transaction of one message") {
         it("is complete with that message") {
-            val trans = Transaction()
-            val transId = id()
-            trans.addMessage(Message(Header(transId, 1, true), Customer(id(), "O")))
-
-            trans.isComplete shouldBe true
+            Transaction()
+                .addMessage(Message(Header(id(), 1, true), Customer(id(), "O")))
+                .isComplete shouldBe true
         }
         it("ignores a message that is added more than once") {
-            val trans = Transaction()
             val transId = id()
             val orgId = id()
             val acn = acn()
-            trans.addMessage(Message(Header(transId, 1, true), Organisation(orgId, acn)))
-            trans.addMessage(Message(Header(transId, 1, true), Organisation(orgId, acn)))
-
-            trans.isComplete shouldBe true
+            Transaction()
+                .addMessage(Message(Header(transId, 1, true), Organisation(orgId, acn)))
+                .addMessage(Message(Header(transId, 1, true), Organisation(orgId, acn)))
+                .isComplete shouldBe true
         }
     }
 
     describe("A transaction of multiple messages") {
         it("is complete when all messages are added in any order") {
-            val trans = Transaction()
             val transId = id()
             val custId = id()
             listOf(
@@ -49,17 +45,16 @@ class TransactionTest : DescribeSpec({
                 Message(Header(transId, 2, false), Organisation(custId, acn())),
                 Message(Header(transId, 3, true), OrganisationName(custId, "2021-05-26", "Pickups P/L")),
             ).shuffled()
-                .forEach(trans::addMessage)
-
-            trans.isComplete shouldBe true
+                .fold(Transaction()) { trans, mess -> trans.addMessage(mess) }
+                .isComplete shouldBe true
         }
         it("throws IllegalStateException if a message with a different transaction ID is added") {
-            val trans = Transaction()
             val transId1 = id()
             val transId2 = id()
             val exception = shouldThrow<IllegalStateException> {
-                trans.addMessage(Message(Header(transId1, 1, false), Customer(id(), "O")))
-                trans.addMessage(Message(Header(transId2, 1, true), Customer(id(), "O")))
+                Transaction()
+                    .addMessage(Message(Header(transId1, 1, false), Customer(id(), "O")))
+                    .addMessage(Message(Header(transId2, 1, true), Customer(id(), "O")))
             }
             exception.message shouldBe "Attempted to add message with transaction ID $transId2 to transaction $transId1"
         }
